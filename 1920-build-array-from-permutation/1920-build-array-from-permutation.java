1e2f327ef9acba80bc418939d93b1ba84ce12ccd
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-05 01:52:54 -0500

    Time: 3 ms (15.95%), Space: 49.8 MB (26.99%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -5,13 +5,9 @@ class Solution {
         //return ans;
         
         //int n = nums.length;
-        for(int i = 0; i<nums.length; i++){
-            nums[i] = nums[i]+(nums.length*(nums[nums[i]]%nums.length));
-        }
+        for(int i = 0; i<nums.length; i++) nums[i] = nums[i]+(nums.length*(nums[nums[i]]%nums.length));
         
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = nums[i] / nums.length;
-        }
+        for(int i = 0; i < nums.length; i++) nums[i] = nums[i] / nums.length;
         
         return nums;
     }
