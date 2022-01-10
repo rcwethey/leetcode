005ae0f913c6c1924c55cc912bf88827e54f326c
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-10 18:03:47 -0500

    Time: 8 ms (73.61%), Space: 41.9 MB (5.13%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -9,22 +9,5 @@ class Solution {
         for(int i = nums.length-1; i>=0; i--) store.put(temp[i], i);
         for(int i = 0; i < nums.length; i++) nums[i] = store.get(nums[i]);
         return nums;
-        
-        /*
-        int[] answerArr = new int[nums.length];
-        int numsLarger = 0;
-        int numberToCompare = 0;
-        while(numberToCompare < nums.length){
-            
-            for(int i = 0;i<nums.length; i++){
-                if(nums[i] < nums[numberToCompare] && i != numberToCompare) 
-                    numsLarger++;
-            }
-            answerArr[numberToCompare] = numsLarger;
-            numsLarger = 0;
-            numberToCompare++;  
-        }
-        return answerArr;
-        */
     }
 }
