f799f756610d241a23108fa685b295d10983b4af
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-11 14:26:01 -0500

    Time: 1 ms (100.00%), Space: 38.7 MB (52.46%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -4,9 +4,7 @@ class Solution {
         Arrays.sort(students);
         
         int ans = 0;
-        for(int i = 0; i<seats.length; i++){
-            ans += Math.abs(seats[i]-students[i]);
-        }
+        for(int i = 0; i<seats.length; i++) ans += Math.abs(seats[i]-students[i]);
         return ans;
         
     }
