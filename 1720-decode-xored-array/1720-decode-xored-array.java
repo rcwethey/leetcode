7e3db443b4ba0809fd0d1b1ab6ce022d54b152e6
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-10 19:37:37 -0500

    Time: 1 ms (100.00%), Space: 40.1 MB (50.15%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.java
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -2,7 +2,8 @@ class Solution {
     public int[] decode(int[] encoded, int first) {
         int[] finaList = new int[encoded.length+1];
         finaList[0] = first;
-        for(int i = 0; i< encoded.length; i++) finaList[i+1] = finaList[i]^encoded[i];
+        for(int i = 0; i< encoded.length; i++) 
+            finaList[i+1] = finaList[i]^encoded[i];
         return finaList;
     }
 }
