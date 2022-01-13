e8dfc338f2793ae782ddf285c354df3753dcbd1c
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-12 20:00:20 -0500

    Time: 2 ms (6.64%), Space: 38.5 MB (25.63%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int sum = 0, count = 0;
+        for(int i = 0; i < s.length(); i++){
+            char current = s.charAt(i);
+            if(current == 'R') count++;
+            if(current == 'L') count--;
+            if(count == 0) sum++;
+        }
+        
+        return sum;
+    }
+}
