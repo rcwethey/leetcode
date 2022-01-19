9eb6089b93e19b5c0659cd16cf8baaa81069d2d5
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-19 15:40:05 -0500

    Time: 16 ms (8.64%), Space: 39 MB (33.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,5 +1,12 @@
 class Solution {
     public char findTheDifference(String s, String t) {
+        /*
+        I could have thought about the Strings as possible totals becuase each character has a value
+        and the difference of both sums then set to a char would hace given me the char added.
+        
+        But I will leave this answer because this is how I got it without the help of anyone else.
+        */
+        
         Map<Character, Integer> sMap = new HashMap<>();
         Map<Character, Integer> tMap = new HashMap<>();
         char theChar = ' ';
@@ -15,11 +22,7 @@ class Solution {
         if(tMap.containsKey(t.charAt(t.length()-1))) tMap.put(t.charAt(t.length()-1), tMap.get(t.charAt(t.length()-1))+1);
         else tMap.put(t.charAt(t.length()-1), 0);
         
-        for(Character c: tMap.keySet()){
-            if(!sMap.containsKey(c) || tMap.get(c) != sMap.get(c)) theChar = c;
-            //if(tMap.get(c) != sMap.get(c)) return c;
-        }
-        
+        for(Character c: tMap.keySet()) if(!sMap.containsKey(c) || tMap.get(c) != sMap.get(c)) theChar = c;
         
         return theChar;
     }
