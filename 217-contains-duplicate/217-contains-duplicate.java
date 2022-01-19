d08b6a85525cf9e8757886a9f8f2685569c6c95e
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-19 11:43:22 -0500

    Time: 20 ms (18.58%), Space: 48.7 MB (73.96%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,20 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> finalSet = new HashSet<>();
+        
         ArrayList<Integer> numsList = new ArrayList<>();
         for(int el: nums) numsList.add(el);
+        
         finalSet.addAll(numsList);
+        
         if(finalSet.size() != nums.length) return true;
         return false;
-        
-        
-        //List<Integer> finalList = new ArrayList<>();
-        
-        //for(int el: nums){
-        //    if(finalList.contains(el)) return true;
-        //    else finalList.add(el);
-        //}
-        
-        //return false;
     }
 }
