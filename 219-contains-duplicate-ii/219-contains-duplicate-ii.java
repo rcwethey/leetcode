a38673e59042c0e1ec625c83d6a2aad31bfc3d4b
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-19 12:53:39 -0500

    Time: 18 ms (72.06%), Space: 54.5 MB (46.84%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if(set.size() > k)
+                set.remove(nums[i-k]);
+        }
+        return false;
+        
+        
+        /*
         for(int i = 0; i< nums.length;i++){
             for(int j = i+1; j <nums.length && j<=(i+k); j++){
                 //System.out.println(i+k + " : " + j);
@@ -8,5 +19,6 @@ class Solution {
             }
         }
         return false;
+        */
     }
 }
