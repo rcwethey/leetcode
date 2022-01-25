a53bd27de9b55bb52056a08397268d460abf9686
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-25 13:18:43 -0500

    Time: 0 ms (100.00%), Space: 35.7 MB (69.85%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
@@ -5,10 +5,6 @@
  *			      1 if num is higher than the guess number
  *               otherwise return 0
  * int guess(int num);
- * time complexity: O(log n)
- * time: 0ms
- * space complexity: n
- * space: 35.8 mb
  */
 
 public class Solution extends GuessGame {
