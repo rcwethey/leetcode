a9f9a17c50e3b5ae164725b0d4d65c1479b34852
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-27 11:36:56 -0500

    Time: 2 ms (96.86%), Space: 42.4 MB (14.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,13 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> tMap = new HashMap<>();
-        Map<Character, Integer> sMap = new HashMap<>();
+        //Map<Character, Integer> tMap = new HashMap<>();
+        //Map<Character, Integer> sMap = new HashMap<>();
         
-        for(char char1 : t.toCharArray()) tMap.put(char1, tMap.getOrDefault(char1, 0)+1);
-        for(char char2 : s.toCharArray()) sMap.put(char2, sMap.getOrDefault(char2,0)+1);
+        //for(char char1 : t.toCharArray()) tMap.put(char1, tMap.getOrDefault(char1, 0)+1);
+        //for(char char2 : s.toCharArray()) sMap.put(char2, sMap.getOrDefault(char2,0)+1);
         //System.out.println(tMap.get('a') +" : "+ sMap.get('a'));
         
-        return tMap.equals(sMap);
+        //return tMap.equals(sMap);
         //return false;
+        
+        //Arrays.sort(s.toCharArray());
+        //Arrays.sort(t.toCharArray());
+        
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+        
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+        
+        return Arrays.equals(sChar, tChar);
     }
 }
