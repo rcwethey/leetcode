abb4e0fc202133f6e1fc7a9a2e797c5b728cc791
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-27 10:49:46 -0500

    Time: 3 ms (21.11%), Space: 42.4 MB (9.12%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,7 +1,10 @@
 class Solution {
     public int addDigits(int num) {
         while(Integer.toString(num).length() > 1){
+        //while(num > 9){
             int total = 0;
+            //total = num%10; <- This gets the last digit in a base-10 number
+            //num = num/10; <- This removes the last digit in a base-10 number
             char[] numCharArray = Integer.toString(num).toCharArray();
             for(char charNum : numCharArray) total+= charNum -'0';
             num = total;
