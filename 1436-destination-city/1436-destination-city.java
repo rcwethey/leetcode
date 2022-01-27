67c730333a5542a8d51d45ab63a877cff8a4dd65
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-27 14:43:01 -0500

    Time: 2 ms (77.24%), Space: 42.1 MB (6.44%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String destCity(List<List<String>> paths){
+        List<String> firstStops = new ArrayList<>();
+        String destination = "";
+        for(int i=0; i<paths.size(); i++){
+            firstStops.add(paths.get(i).get(0));
+        }
+        
+        for(int j=0; j<paths.size(); j++){
+            if(firstStops.indexOf(paths.get(j).get(1)) == -1) destination = paths.get(j).get(1);
+        }
+        
+        return destination;
+    }
+}
