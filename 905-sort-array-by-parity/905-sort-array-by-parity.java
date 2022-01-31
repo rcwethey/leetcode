973f00700e9fee5439490493dc78f061f134fab2
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-31 16:56:17 -0500

    Time: 2 ms (44.02%), Space: 48.6 MB (5.06%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int lp = 0, rp = nums.length-1;
+        while(lp < rp){
+            while(lp < nums.length && nums[lp] % 2 == 0) lp++;
+            while(rp > 0 && nums[rp] % 2 == 1) rp--;
+            if((lp < nums.length && rp > 0) && lp != rp+1){
+                swap(nums, lp, rp);
+            }
+            lp++;
+            rp--;
+        }
+        
+        return nums;
+    }
+    
+    public void swap(int[] nums, int lp, int rp){
+        //System.out.println(lp + " : " + rp);
+        int temp = nums[lp];
+        nums[lp] = nums[rp];
+        nums[rp] = temp;
+    }
+    
+    
+}
