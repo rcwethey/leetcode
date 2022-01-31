89541b748718bc5c6a712ab3bd1ecedff4dc9abb
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-31 11:50:03 -0500

    Time: 12 ms (17.06%), Space: 42.9 MB (6.57%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<String> finalSummaryRanges;
+    int leftPointer = 0;
+    int rightPointer = 0;
+    
+    public List<String> summaryRanges(int[] nums) {
+        finalSummaryRanges = new ArrayList<>();
+        if(nums.length == 0) return finalSummaryRanges;
+        
+        for(int i = 0; i<nums.length; i++){
+            if(rightPointer+1 < nums.length && nums[rightPointer+1] == nums[rightPointer]+1) rightPointer++;
+            else addtoArrayList(nums);
+        }
+        
+        return finalSummaryRanges;
+    }
+    
+    public void addtoArrayList(int[] nums){
+        if(leftPointer < rightPointer) 
+            finalSummaryRanges.add(Integer.toString(nums[leftPointer])+"->"+Integer.toString(nums[rightPointer]));
+        else finalSummaryRanges.add(Integer.toString(nums[rightPointer]));
+        
+        rightPointer++;
+        leftPointer = rightPointer;
+    }
+}
