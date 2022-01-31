cf03a71545e70a30948224c09b28ceb477e10996
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-31 12:10:10 -0500

    Time: 2 ms (18.72%), Space: 41.9 MB (5.02%) - LeetHub

---
+++ 551-student-attendance-record-i/551-student-attendance-record-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        if(s.length() < 2) return true;
+        
+        int absence = 0, maxLate = 0, latesInARow = 0;
+        boolean lateStreak = false;
+        
+        for(char attendenceCheck: s.toCharArray()){
+            if(attendenceCheck == 'A') absence++;
+            if(attendenceCheck == 'L'){
+                lateStreak = true;
+                latesInARow++;
+            }
+            if(attendenceCheck != 'L' && lateStreak){
+                lateStreak = false;
+                if(latesInARow > maxLate) maxLate = latesInARow;
+                latesInARow = 0;
+            }
+        }
+        
+        if(latesInARow > maxLate) maxLate = latesInARow;
+        return (absence < 2 && maxLate < 3);
+    }
+}
