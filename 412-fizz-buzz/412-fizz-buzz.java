c647ec4f2b99934d69e945e109ad50c8dba671af
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-31 10:55:21 -0500

    Time: 1 ms (99.80%), Space: 48.7 MB (5.25%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -2,6 +2,8 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> fizzBuzzList = new ArrayList<>();
         
+        
+        
         for(int i = 1; i <= n; i++){
             if(i % 3 == 0 && i % 5 == 0) fizzBuzzList.add("FizzBuzz");
             else if(i % 3 == 0) fizzBuzzList.add("Fizz");
