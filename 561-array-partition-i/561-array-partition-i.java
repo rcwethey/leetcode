a262bd6bfbd10635a27ab4208f2c284288455467
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-01-31 16:33:03 -0500

    Time: 16 ms (43.08%), Space: 55 MB (5.13%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.java
+++ 561-array-partition-i/561-array-partition-i.java
@@ -1,10 +1,8 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-    
         int maxSum = 0;
         for(int i=0; i<nums.length-1; i+=2) maxSum += nums[i];
-        
         return maxSum;
     }
 }
