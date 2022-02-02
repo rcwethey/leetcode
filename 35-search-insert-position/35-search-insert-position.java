2ec48efbe38bbebba4fa37969c045c1c80cfbe7f
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-02 14:46:18 -0500

    Time: 0 ms (100.00%), Space: 43.7 MB (5.28%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        return logSearch(nums, start, end, target);
+    }
+    
+    public int logSearch(int[] nums, int start, int end, int target){
+        int midIndex = (int)Math.floor((start+end)/2);
+        
+        if(nums[midIndex] == target) return midIndex;
+        if(start >= end) return (target < nums[start]) ? start : start+1;
+        
+        if(target < nums[midIndex]) return logSearch(nums, start, midIndex-1, target);
+        else return logSearch(nums, midIndex+1, end, target);
+    }    
+    
+}
