dbefc13f7d479e09fb22899c57578a67f3c17b24
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-07 19:38:33 -0500

    Time: 1 ms (100.00%), Space: 44.2 MB (60.30%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,9 +1,29 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        PriorityQueue<Integer> newQueue = new PriorityQueue<>();
+        //PriorityQueue<Integer> newQueue = new PriorityQueue<>();
         int[] solution = new int[nums.length];
-        for(int i = 0; i< nums.length; i++) newQueue.add(nums[i]*nums[i]);
-        for(int j = 0; j<nums.length; j++) solution[j] = newQueue.poll();
+        
+        int i = 0, j = nums.length-1;
+        int index = solution.length-1;
+        
+        while(i<=j){
+            int val1 = nums[i]*nums[i];
+            int val2 = nums[j]*nums[j];
+            
+            if(val1 < val2){
+                solution[index] = val2;
+                j--;
+            }
+            else{
+                solution[index] = val1;
+                i++;
+            }
+            index--;
+            
+        }
+        
+        //for(int i = 0; i< nums.length; i++) newQueue.add(nums[i]*nums[i]);
+        //for(int j = 0; j<nums.length; j++) solution[j] = newQueue.poll();
         
         return solution;
     }
