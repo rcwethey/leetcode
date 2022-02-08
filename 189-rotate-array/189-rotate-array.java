a3e444188f500008b6c5692d1ccbab9968e5e8ae
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-08 12:58:11 -0500

    Time: 2 ms (45.04%), Space: 64.7 MB (12.32%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] solution = new int[nums.length];
+        
+        if(nums.length > 1 && k != 0){
+            for(int i = 0; i<nums.length; i++){
+                int newVal = i+k;
+                while(newVal > nums.length-1) newVal -= nums.length;
+                solution[newVal] = nums[i];
+            }
+
+            for(int j = 0; j<nums.length; j++) nums[j] = solution[j];
+        }
+    }
+}
