713aa861204c09fb03286d766d566a1cb09cd568
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-08 13:05:58 -0500

    Time: 2 ms (45.04%), Space: 64.8 MB (9.49%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -9,14 +9,16 @@ class Solution {
         4. reverse k t0 length-1 in array 
         */
         
-        
-        
+        // O(n) space and 
         int[] solution = new int[nums.length];
         
         if(nums.length > 1 && k != 0){
+            //k = k%nums.length;
             for(int i = 0; i<nums.length; i++){
                 int newVal = i+k;
                 while(newVal > nums.length-1) newVal -= nums.length;
+                //if(i+k > nums.length) solution[i+k-nums.length] = nums[i];
+                //else 
                 solution[newVal] = nums[i];
             }
 
