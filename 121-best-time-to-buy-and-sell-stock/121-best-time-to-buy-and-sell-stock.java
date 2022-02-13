69519f463a3ff0156626db3496f9cde7b429c0ce
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-12 20:12:18 -0500

    Time: 2 ms (83.92%), Space: 85.1 MB (38.18%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,24 @@
 class Solution {
-    /*
+        
+    //[2,1,4]
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        int min=Integer.MAX_VALUE;
+        for(int price : prices){
+            //Same thing as saying -> if(price < min) min = price; 
+            min=Math.min(min,price);
+            
+            //Same thing as saying -> if(price-min > profit) profit = price-min; 
+            profit=Math.max(profit,price-min);
+         }
+
+        return profit;
+    }
+    
+    /* 
+    
+    ---WORKING SOLUTION---
+    
     public int maxProfit(int[] prices) {
         int lp = 0, rp = 1, max = 0;
        
@@ -20,38 +39,24 @@ class Solution {
     }
     */
         
+    /*
+    
+    ---NON WORKING SOLUTION---
+    
+    int max = 0, lp = 0, rp = prices.length-1;
+
     //[2,1,4]
-    public int maxProfit(int[] prices) {
-        int profit=0;
-        int min=Integer.MAX_VALUE;
-        for(int price : prices){
-            //Same thing as saying -> 
-            if(price < min) min = price; 
-            //min=Math.min(min,price);
-            
-            //Same thing as saying -> 
-            if(price-min > profit) profit = price-min; 
-            //profit=Math.max(profit,price-min);
-         }
-
-        return profit;
-    }
-        
-        /*
-        int max = 0, lp = 0, rp = prices.length-1;
-
-        //[2,1,4]
-        while(lp < rp){
-            int difference = 0;
-            if(prices[lp] > prices[rp]) lp++;
-            else{
-                difference = prices[rp] - prices[lp];
-                rp--;
-            }
-
-            if(difference > max) max = difference;
+    while(lp < rp){
+        int difference = 0;
+        if(prices[lp] > prices[rp]) lp++;
+        else{
+            difference = prices[rp] - prices[lp];
+            rp--;
         }
 
-        return max;
-        */
+        if(difference > max) max = difference;
+    }
+
+    return max;
+    */
 }
