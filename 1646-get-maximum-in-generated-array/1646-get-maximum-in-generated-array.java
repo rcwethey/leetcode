d7c06469e33cf42a98f504f7fc2521df225580bc
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-14 14:28:41 -0500

    Time: 2 ms (10.70%), Space: 41.8 MB (5.05%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    int[] arr;
+    public int getMaximumGenerated(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        arr = new int[n+1];
+        createArray(arr);  
+        
+        Arrays.sort(arr);
+        return arr[n];
+    }
+    
+    void createArray(int[] arr){
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+        for(int i = 3; i<arr.length; i++){
+            if(i % 2 == 0) arr[i] = arr[i/2];
+            else arr[i] = arr[(int)i/2] + arr[(int)i/2+1];
+        }
+    }
+}
