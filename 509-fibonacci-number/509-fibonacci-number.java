3bc3a0345fda9605a1c94a75f33097128372fd12
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-14 14:16:06 -0500

    Time: 0 ms (100.00%), Space: 40.7 MB (17.64%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,9 @@
 class Solution {
     int[] arr;
+    
     public int fib(int n) {
-        /*
-        if(n > 2) return fib(n-1) + fib(n-2);
-        if(n == 1 || n == 2) return 1;
-        else return 0;
-        */
-        
-        //create an array and compute all the numbers up to n+1 length using fib sequence
-        //return array pos n
         if(n == 0) return 0;
-        if(n == 1 || n== 2) return 1;
+        if(n==1 || n==2) return 1;
         arr = new int[n+1];
         createArray(arr);
         return arr[n];
@@ -21,8 +14,6 @@ class Solution {
         arr[1] = 1;
         arr[2] = 1;
         
-        for(int i = 3; i<arr.length; i++){
-            arr[i] = arr[i-1] + arr[i-2];
-        }
+        for(int i = 3; i<arr.length; i++) arr[i] = arr[i-1] + arr[i-2];
     }
 }
