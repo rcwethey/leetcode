8e31e5df7ca270824e69bc1ea74d1d865f597774
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-14 13:25:01 -0500

    Time: 1 ms (78.01%), Space: 43.3 MB (16.22%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> sol = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        //List<Integer> sublist = new ArrayList<>();
+        helper(nums, 0, new ArrayList<Integer>());
+        return sol;
+    }
+    
+    public void helper(int[] nums, int start, List<Integer> sublist){
+        
+        if(start == nums.length){
+            sol.add(new ArrayList<>(sublist));
+            return;
+        }
+        
+        int val = nums[start];
+        sublist.add(val);
+        helper(nums, start+1, sublist);
+        sublist.remove((Integer) val);
+        helper(nums, start+1, sublist);
+    }
+}
