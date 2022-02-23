b7766e6d7d9f56d07594c3d666c6983e5fe2da0b
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-23 15:08:12 -0500

    Time: 2 ms (71.98%), Space: 41.6 MB (60.61%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.java
@@ -4,9 +4,9 @@ class Solution {
         int lengthOfArray = columnTitleCharArray.length-1;
         int sum = columnTitleCharArray[lengthOfArray]-64;
         for(int i = lengthOfArray-1; i>=0;i--){
-            int asciiVal = columnTitleCharArray[i];
+            //int asciiVal = columnTitleCharArray[i];
             int multiplier = (int)(Math.pow(26,lengthOfArray-i));
-            sum +=  multiplier * (asciiVal - 64);
+            sum +=  multiplier * (columnTitleCharArray[i] - 64);
         }
         return sum;  
     }
