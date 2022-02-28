bb716f1c8827522f0176d3f2c79da0652b30a50e
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-02-28 12:09:06 -0500

    Time: 2 ms (59.67%), Space: 51.2 MB (25.87%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,7 +1,49 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        
+        int sum = 0;
+        for(int num : nums){
+            sum ^= num;
+        }
+        
+        return sum;
+        
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*   
         short number = (short)nums[0];
         for(int i=1;i<nums.length;i++) number ^= nums[i];
         return number;
+    */
     }
 }
