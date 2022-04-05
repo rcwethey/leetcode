b993f9db1f1c939335d6f05bc0bec5a80aae2abd
Ryan Wethey <54594769+rcwethey@users.noreply.github.com>
2022-04-05 17:04:43 -0400

    Time: 14 ms (19.14%), Space: 42.1 MB (12.56%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            if(i<2) dp[i] = Math.max(nums[i],dp[i-1]);
+            else dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        for(int i = 0; i<dp.length; i++) System.out.print(dp[i]+",");
+        return dp[n-1];
+    }
+}
